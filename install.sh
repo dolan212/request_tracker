2558e6e222007733a1787a3e5d5b7e9c53ba5fa3
Dylan Draper <u16081758@tuks.co.za>
2017-03-27 21:30:54 +0200

    Updated scripts
    Now actually working!

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 VE_NAME="dylan_ve"
 
-if [! -d "$VE_NAME" ]; then
+if [ ! -d "$VE_NAME" ]; then
 	virtualenv -q $VE_NAME --no-site-packages
-	echo "Virtualenv created with name $VE_NAME."
+	echo "Virtualenv created in current folder with name $VE_NAME."
 fi
 
 . $VE_NAME/bin/activate
-pip install -r requirements.txt $VE_NAME
+pip install -r requirements.txt
 echo "Requirements installed."
 echo $VE_NAME > .venv
 
